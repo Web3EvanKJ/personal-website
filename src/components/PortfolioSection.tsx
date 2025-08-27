83b904714ec3c45a4e7ce86cb12ab65fa3446d74
Nave765 <web3evankeane@gmail.com>
2025-08-27 20:48:07 +0700

    Fix: Nave Bank

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -10,14 +10,14 @@ export default function PortfolioSection() {
 
       {/* Example project */}
       <div className="bg-white shadow-lg rounded-lg p-6 space-y-4">
-        <h3 className="text-xl font-semibold">Banking Platform OCBC</h3>
+        <h3 className="text-xl font-semibold">Banking Platform Nave Bank</h3>
         <p className="text-sm text-gray-500">Technologies: NextJS, Tailwind</p>
 
         <div>
           <h4 className="font-bold">Situation:</h4>
           <p>
-            OCBC wanted a scalable, mobile-friendly banking platform to boost
-            sales and streamline inventory.
+            Nave Bank wanted a scalable, mobile-friendly banking platform to
+            boost sales and streamline inventory.
           </p>
         </div>
 
@@ -44,7 +44,7 @@ export default function PortfolioSection() {
 
         {/* Button linking to project */}
         <Link
-          href="https://company-profile-ocbc.vercel.app/"
+          href="https://company-profile-nave-bank.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
           className="inline-flex items-center justify-center rounded-md bg-green-600 px-4 py-2 text-white text-sm font-medium shadow hover:bg-green-700 transition"
@@ -52,8 +52,8 @@ export default function PortfolioSection() {
           View Project
         </Link>
         <Image
-          src="/ocbc-project.png"
-          alt="Banking Platform OCBC"
+          src="/nave-bank-project.png"
+          alt="Banking Platform Nave"
           width={500}
           height={300}
           className="min-w-full"
