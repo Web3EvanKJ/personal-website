63778100459cc20c7c7ca23a608c1cc1b8dc57ee
Nave765 <web3evankeane@gmail.com>
2025-11-06 22:16:00 +0700

    Update

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -8,34 +8,6 @@ export default function PortfolioSection() {
         Portfolio
       </h2>
 
-      {/* Example project */}
-      <div className="bg-white shadow-lg rounded-lg p-6 space-y-4">
-        <h3 className="text-xl font-bold">Banking Platform Nave Bank</h3>
-        <p className="text-sm text-gray-500">Technologies: NextJS, Tailwind</p>
-
-        <p>
-          Develop a website with smooth UX and fast performance company profile.
-          Nave Bank wanted a scalable, mobile-friendly banking platform to boost
-          sales and streamline inventory.
-        </p>
-
-        {/* Button linking to project */}
-        <Link
-          href="https://company-profile-nave-bank.vercel.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-flex items-center justify-center rounded-md bg-sky-600 px-4 py-2 text-white text-sm font-medium shadow hover:bg-sky-700 transition"
-        >
-          View Project
-        </Link>
-        <Image
-          src="/nave-bank-project.png"
-          alt="Banking Platform Nave"
-          width={500}
-          height={300}
-          className="min-w-full"
-        />
-      </div>
       <div className="bg-white shadow-lg rounded-lg p-6 space-y-4">
         <h3 className="text-xl font-bold">Jagantara</h3>
         <p className="text-sm text-gray-500">Technologies: Solidity, NextJS</p>
@@ -59,6 +31,34 @@ export default function PortfolioSection() {
         <Image
           src="/jagantara.png"
           alt="Jagantara"
+          width={500}
+          height={300}
+          className="min-w-full"
+        />
+      </div>
+      {/* Example project */}
+      <div className="bg-white shadow-lg rounded-lg p-6 space-y-4">
+        <h3 className="text-xl font-bold">Banking Platform Nave Bank</h3>
+        <p className="text-sm text-gray-500">Technologies: NextJS, Tailwind</p>
+
+        <p>
+          Develop a website with smooth UX and fast performance company profile.
+          Nave Bank wanted a scalable, mobile-friendly banking platform to boost
+          sales and streamline inventory.
+        </p>
+
+        {/* Button linking to project */}
+        <Link
+          href="https://company-profile-nave-bank.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center justify-center rounded-md bg-sky-600 px-4 py-2 text-white text-sm font-medium shadow hover:bg-sky-700 transition"
+        >
+          View Project
+        </Link>
+        <Image
+          src="/nave-bank-project.png"
+          alt="Banking Platform Nave"
           width={500}
           height={300}
           className="min-w-full"
