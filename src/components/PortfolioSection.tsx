5712b5709aa92dc7f3a6ed960d4839f64371e1e4
Nave765 <web3evankeane@gmail.com>
2025-11-25 19:33:34 +0700

    Add: grocify

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -64,6 +64,44 @@ export default function PortfolioSection() {
           className="min-w-full"
         />
       </div>
+      {/* Example project */}
+      <div className="bg-white shadow-lg rounded-lg p-6 space-y-4">
+        <h3 className="text-xl font-bold">
+          E-commerce Multi Warehouse Grocify
+        </h3>
+        <p className="text-sm text-gray-500">
+          Technologies: NextJS, Tailwind, Express, PostgreSQL, Prisma
+        </p>
+
+        <p>
+          Online Grocery Web App where users shop from the nearest store branch
+          based on their real-time location. The system displays products and
+          stock from the closest store within a service radius and notifies
+          users if they are outside the coverage area. Users must create an
+          account to make purchases, while super admins manage store admins and
+          store admins handle inventory and order processing. Orders are routed
+          to the appropriate branch and follow a full order-status flow. The app
+          also includes customizable discounts such as product-based deals,
+          vouchers, referral rewards, and free-shipping benefits.
+        </p>
+
+        {/* Button linking to project */}
+        <Link
+          href="https://grocery-app-2-frontend.vercel.app/admin/dashboard"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center justify-center rounded-md bg-sky-600 px-4 py-2 text-white text-sm font-medium shadow hover:bg-sky-700 transition"
+        >
+          View Project
+        </Link>
+        <Image
+          src="/grocify.png"
+          alt="Ecommerce Multi Warehouse Grocify"
+          width={500}
+          height={300}
+          className="min-w-full"
+        />
+      </div>
     </section>
   );
 }
